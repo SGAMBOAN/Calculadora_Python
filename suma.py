28f577d77a5ad4a8ed13576a51ef52c821c6fbc8
SGAMBOAN <rrhh.opera17@gmail.com>
2022-06-07 22:41:39 -0400

    ingresando sumas y restas

---
+++ suma.py
@@ -0,0 +1,13 @@
+num1 = input("Introduce el primer numero: ")
+num2 = input("Introduce el segundo numero: ")
+num3 = input("Introduce el tercer numero: ")
+
+num1  = int(num1)
+num2 = int(num2)
+num3 = int(num3)
+
+suma = num1+num2+num3
+resta = num1-num2-num3
+
+print ("La suma de los tres números es:", suma)
+print ("La resta de los tres números es:", resta)
